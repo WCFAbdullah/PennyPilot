3877010df8b82d6e47ef2f1b53d356f0e1259ab7
taha-shahid-1 <tahashahid246@gmail.com>
2025-01-11 20:31:13 -0500

    Added sign-up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,41 @@
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import Dashboard from "./pages/Dashboard";
-import SignInPage from "./pages/SignInPage";
-import Navbar from "./components/Navbar";
+import LandingPage from "./webpages/landingpage";
+import Dashboard from "./webpages/dashboard";
+import SignInPage from "./webpages/signin";
+import Navbar from "./webpages/navbar";
+import SignUpPage from "./webpages/signup";
 
 const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 function App() {
   return (
-    <ClerkProvider publishableKey={publishableKey}>
-      <BrowserRouter>
-        <Navbar /> 
-        <Routes>
-          
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/sign-in" element={<SignInPage />} />
-          
-          <Route
-            path="/dashboard"
-            element={
-              <SignedIn>
-                <Dashboard />
-              </SignedIn>
-            }
-          />
-          
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      {/* Public routes accessible to all */}
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/sign-in" element={<SignInPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} />  {/* Add sign-up route */}
+      
+      {/* Protected route */}
+      <Route
+        path="/dashboard"
+        element={
+          <>
+            <SignedIn>
+              <Dashboard />
+            </SignedIn>
+            <SignedOut>
+              <Navigate to="/sign-in" />
+            </SignedOut>
+          </>
+        }
+      />
+      
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ frontend/src/webpages/signup.jsx
@@ -0,0 +1,15 @@
+import { SignUp } from "@clerk/clerk-react";
+
+function SignUpPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <SignUp 
+        routing="path" 
+        path="/sign-up" 
+        redirectUrl="/dashboard"
+      />
+    </div>
+  );
+}
+
+export default SignUpPage;

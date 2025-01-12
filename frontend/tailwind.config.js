41eb2f20e83af07fa0f21819b84862e21c3b2055
taha-shahid-1 <tahashahid246@gmail.com>
2025-01-11 19:38:51 -0500

    Set up frontend folder for React and TailwindCSS

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], // Include your source files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
